37febadec6e5be80a4a4db0e6787d4242aca41da
Aset <asetgabdullov97@gmail.com>
2020-09-16 16:00:57 +0600

    login redirect to cabinet fix 10

--- src/views/empty/components/Login.vue
+++ src/views/empty/components/Login.vue
@@ -60,9 +60,9 @@ export default {
                 .then((response) => {
                     if (response.status == 200) {
                         if (response.data.is_entity == 1) {
-                            document.cookie = "token="+ response.data.token + "; domain=.eqonaq.kz; max-age=3600"
+                            document.cookie = "token="+ response.data.token + "; domain=.eqonaq.kz; max-age=30"
                             window.location.href = 'https://cabinet.stage.eqonaq.kz'
-                            // document.cookie = "token="+ response.data.token + "; domain=localhost; max-age=3600"
+                            // document.cookie = "token="+ response.data.token + "; domain=localhost; max-age=30"
                             // window.location.href = 'http://localhost:8081/login' 
                         } else {
                             this.SIGN_IN_USER(response.data.token)
