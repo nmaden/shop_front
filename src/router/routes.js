9132ac9904646fe78a04afd29f0bec834f98d5d1
Nurbol Madenov <nurbolmaden@gmail.com>
2021-03-25 10:27:30 +0500

    first commit

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,34 +6,8 @@ export const routes = [
       name: 'Main',
       component: () => import('../views/Main.vue'),
     },
-    {
-      path: '/password/:hash',
-      name: 'Password',
-      component: () => import('../views/empty/Password.vue'),
-      meta: {layout: 'not_authorized', requiresAuth: false},
-      // beforeEnter: authGuard
-    },
-    {
-      path: '/news',
-      name: 'NewsAll',
-      component: () => import('../views/empty/NewsAll.vue'),
-      meta: {layout: 'not_authorized', requiresAuth: false},
-      // beforeEnter: authGuard
-    },
-    {
-      path: '/news-detail-:id',
-      name: 'NewsDetail',
-      component: () => import('../views/empty/NewsDetail.vue'),
-      meta: {layout: 'not_authorized', requiresAuth: false},
-      // beforeEnter: authGuard
-    },
-    {
-      path: '/forgot',
-      name: 'Forgot',
-      component: () => import('../views/empty/ForgotPassword.vue'),
-      meta: {layout: 'not_authorized', requiresAuth: false},
-      beforeEnter: authGuard
-    },
+ 
+
     {
       path: '/login',
       name: 'GreenLogin',
@@ -48,63 +22,9 @@ export const routes = [
       meta: {layout: 'not_authorized', requiresAuth: false},
       beforeEnter: authGuard
     },
-    {
-      path: '/regitstration',
-      name: 'Regitstration',
-      component: () => import('../views/empty/Regitstration.vue'),
-      meta: {layout: 'not_authorized', requiresAuth: false},
-      beforeEnter: authGuard
-    },
-    {
-      path: '/entityregistration',
-      name: 'EntityRegistration',
-      component: () => import('../views/empty/EntityRegistration.vue'),
-      meta: {layout: 'not_authorized', requiresAuth: false},
-      beforeEnter: authGuard
-    },
-    {
-      path: '/notif',
-      name: 'Notif',
-      component: () => import('../views/cabinet/Notif.vue'),
-      meta: {layout: 'authorized', requiresAuth: true},
-    },
-    {
-      path: '/address',
-      name: 'AddressList',
-      component: () => import('../views/cabinet/components/AddressList.vue'),
-      meta: {layout: 'authorized', requiresAuth: true},
-    },
-    {
-      path: '/profile',
-      name: 'Profile',
-      component: () => import('../views/cabinet/Profile.vue'),
-      meta: {layout: 'authorized', requiresAuth: true},
-    },
-    {
-      path: '/currency',
-      name: 'Сurrency',
-      component: () => import('../views/cabinet/Сurrency.vue'),
-    },
-    {
-      path: '/telephones',
-      name: 'Telephones',
-      component: () => import('../views/cabinet/Telephones.vue'),
-    },
-    {
-      path: '/connection',
-      name: 'Connection',
-      component: () => import('../views/cabinet/Connection.vue'),
-    },
-    {
-      path: '/migration',
-      name: 'Migration',
-      component: () => import('../views/cabinet/Migration.vue'),
-    },
-    {
-      path: '/search',
-      name: 'Search',
-      component: () => import('../views/cabinet/Search.vue'),
-    },
+
+  
+ 
     {
       path: '*',
       name: '404',
